54ab9101691c6e6f485aeeeb6044937e5037f7f3
Rafael da silva kuhn <pontinho@favela.network>
2025-01-03 16:42:30 -0300

    Initial version

--- src/app.service.ts
+++ src/app.service.ts
@@ -151,6 +151,102 @@ throw new Error("Erro ao iniciar chat")
 
 }
 
+async function startFlowTypebotRECUPERACAO(item, codigoRastreio) {
+  var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "prefilledVariables": {
+    "clientEmail": item.customer.email,
+    "clientName": item.customer.name,
+    "clientPhone": item.customer.phone,
+    "clientDocument": item.customer.document.number,
+    "codigoRastreio": codigoRastreio,
+    "cep": item.customer?.address?.zipCode || "70872050",
+    "data": new Date().toLocaleDateString(),
+    "logoUrl": `https://s3.rastreou.org/cod-rastreio/jadlog.png`,
+    "horario": new Date().toLocaleTimeString(),
+    "endereco": item.customer.address?.street,
+    "state": item.customer.address?.state,
+    "valor": (item.amount / 100).toString(),
+    "frete": ((item.shipping?.amount / 100 || 0) + 27.99).toString(),
+    "storeName": "PAGUESEGURO",
+    "urlSecure": item.secureUrl,
+    "productsHtml": item.items.map((item) => {
+      return `
+      <tr>
+<td style="border-collapse: collapse;"></td>
+</tr>
+<tr>
+<td style="border-collapse: collapse; width: 75px;">
+<img src="https://s3.rastreou.org/cod-rastreio/placeholder.png"
+    style="width:50px; border: 2px solid; border-radius: 10px;">
+</td>
+<td style="border-collapse: collapse;">
+<p
+    style="font-size:14px;line-height:20px;margin-top:0;margin-bottom:0;color:#828282">
+    <strong>
+      ${item.title}
+    </strong></p>
+<p
+    style="font-size:14px;line-height:20px;margin-top:0;margin-bottom:0;color:#828282">
+    ${item.quantity} un. x R$&nbsp;${item.unitPrice/100}</p>
+</td>
+</tr>
+
+<tr>
+<td style="border-collapse: collapse;"></td>
+</tr>
+      `
+    }).join("\n")
+  },
+  "isOnlyRegistering": false,
+  "isStreamEnabled": false,
+  "textBubbleContentFormat": "richText"
+});
+
+const resp = await fetch("https://typechat.ads-information.top/api/v1/typebots/taxa-tenf-no-payment-7x6xv4s/startChat", {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+})
+const data = await resp.json()
+if(resp.status == 200){
+  return "ok"
+}
+throw new Error("Erro ao iniciar chat")
+
+}
+
+async function createTransaction(dataT) {
+  var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Basic c2tfbGl2ZV9XdUZIUWJJb0htQVpncElESG40WUJmcUdoakZPSU9GQzloRk5ReE8zT2E6eA==");
+delete dataT.id
+delete dataT.splits
+ dataT.customer.id = undefined
+dataT.items = dataT.items.map((item) => {
+  // 10% de desconto
+ return {
+  ...item,
+  unitPrice: item.unitPrice * 0.9
+ }
+})
+dataT.amount = dataT.amount * 0.9
+var raw = JSON.stringify(dataT)
+
+const response = await fetch("https://api.gateway.cashtimepay.com.br/v1/transactions/", {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+})
+const data = await response.json()
+console.log(data)
+return data
+}
+
 // @ts-ignore
 var agenda = new Agenda({ db: { 
   // address: "mongodb://admin:%40Souumbbk1@185.209.230.133:27017/",
@@ -237,8 +333,8 @@ export class AppService {
         }
 
         await startFlowTypebotTENF(data, data.id)
-        
-        
+        const transaction = await createTransaction(data)
+        await startFlowTypebotRECUPERACAO(transaction, data.id)
 
         if(isPhysical && data.customer?.address?.zipCode){
           // 3 dias
