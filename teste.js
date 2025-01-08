8b2dc5a37e4ee7260ae504de363c1eda20fb5d3a
Rafael da silva kuhn <pontinho@favela.network>
2025-01-08 20:01:53 -0300

    Initial version

---
+++ teste.js
@@ -0,0 +1,584 @@
+const a = [
+    {
+        "title": "SMS FUNNEL - PIX URL",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "SMS_FUNNEL_PIX_URL",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "1",
+        "cdate": "2024-04-26T11:37:56-05:00",
+        "udate": "2024-04-26T11:37:56-05:00",
+        "created_timestamp": "2024-04-26 11:37:56",
+        "updated_timestamp": "2024-04-26 11:37:56",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "1"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/1/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/1/relations"
+        },
+        "id": "1"
+    },
+    {
+        "title": "SMS FUNNEL - CUSTOM URL",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "SMS_FUNNEL_CUSTOM_URL",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "2",
+        "cdate": "2024-04-26T11:37:59-05:00",
+        "udate": "2024-04-26T11:37:59-05:00",
+        "created_timestamp": "2024-04-26 11:37:59",
+        "updated_timestamp": "2024-04-26 11:37:59",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "2"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/2/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/2/relations"
+        },
+        "id": "2"
+    },
+    {
+        "title": "Contato",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "CONTATO",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "3",
+        "cdate": "2024-10-20T22:57:14-05:00",
+        "udate": "2024-10-20T22:57:14-05:00",
+        "created_timestamp": "2024-10-20 22:57:14",
+        "updated_timestamp": "2024-10-20 22:57:14",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "3"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/3/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/3/relations"
+        },
+        "id": "3"
+    },
+    {
+        "title": "checkbox title",
+        "descript": "Here is the description",
+        "type": "checkbox",
+        "isrequired": "0",
+        "perstag": "UNIQUECUSTOMTAG",
+        "defval": null,
+        "show_in_list": "1",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "4",
+        "cdate": "2025-01-08T13:04:29-06:00",
+        "udate": "2025-01-08T13:04:29-06:00",
+        "created_timestamp": "2025-01-08 13:04:29",
+        "updated_timestamp": "2025-01-08 13:04:29",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/4/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/4/relations"
+        },
+        "id": "4"
+    },
+    {
+        "title": "html",
+        "descript": "",
+        "type": "textarea",
+        "isrequired": "0",
+        "perstag": "HTML",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "5",
+        "cols": "40",
+        "visible": "1",
+        "service": "",
+        "ordernum": "5",
+        "cdate": "2025-01-08T13:33:45-06:00",
+        "udate": "2025-01-08T13:33:45-06:00",
+        "created_timestamp": "2025-01-08 13:33:45",
+        "updated_timestamp": "2025-01-08 13:33:45",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "4"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/5/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/5/relations"
+        },
+        "id": "5"
+    },
+    {
+        "title": "productsHtml",
+        "descript": "",
+        "type": "textarea",
+        "isrequired": "0",
+        "perstag": "PRODUCTSHTML",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "5",
+        "cols": "40",
+        "visible": "1",
+        "service": "",
+        "ordernum": "6",
+        "cdate": "2025-01-08T15:36:26-06:00",
+        "udate": "2025-01-08T15:36:26-06:00",
+        "created_timestamp": "2025-01-08 15:36:26",
+        "updated_timestamp": "2025-01-08 15:36:26",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "5"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/6/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/6/relations"
+        },
+        "id": "6"
+    },
+    {
+        "title": "urlSecure",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "URLSECURE",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "7",
+        "cdate": "2025-01-08T15:36:40-06:00",
+        "udate": "2025-01-08T15:36:40-06:00",
+        "created_timestamp": "2025-01-08 15:36:40",
+        "updated_timestamp": "2025-01-08 15:36:40",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/7/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/7/relations"
+        },
+        "id": "7"
+    },
+    {
+        "title": "storeName",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "STORENAME",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "8",
+        "cdate": "2025-01-08T15:36:46-06:00",
+        "udate": "2025-01-08T15:36:46-06:00",
+        "created_timestamp": "2025-01-08 15:36:47",
+        "updated_timestamp": "2025-01-08 15:36:47",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "6"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/8/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/8/relations"
+        },
+        "id": "8"
+    },
+    {
+        "title": "frete",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "FRETE",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "9",
+        "cdate": "2025-01-08T15:36:53-06:00",
+        "udate": "2025-01-08T15:36:53-06:00",
+        "created_timestamp": "2025-01-08 15:36:53",
+        "updated_timestamp": "2025-01-08 15:36:53",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "7"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/9/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/9/relations"
+        },
+        "id": "9"
+    },
+    {
+        "title": "valor",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "VALOR",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "10",
+        "cdate": "2025-01-08T15:36:56-06:00",
+        "udate": "2025-01-08T15:36:56-06:00",
+        "created_timestamp": "2025-01-08 15:36:57",
+        "updated_timestamp": "2025-01-08 15:36:57",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "8"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/10/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/10/relations"
+        },
+        "id": "10"
+    },
+    {
+        "title": "state",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "STATE",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "11",
+        "cdate": "2025-01-08T15:37:01-06:00",
+        "udate": "2025-01-08T15:37:01-06:00",
+        "created_timestamp": "2025-01-08 15:37:01",
+        "updated_timestamp": "2025-01-08 15:37:01",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "9"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/11/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/11/relations"
+        },
+        "id": "11"
+    },
+    {
+        "title": "endereco",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "ENDERECO",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "12",
+        "cdate": "2025-01-08T15:37:09-06:00",
+        "udate": "2025-01-08T15:37:09-06:00",
+        "created_timestamp": "2025-01-08 15:37:09",
+        "updated_timestamp": "2025-01-08 15:37:09",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "10"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/12/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/12/relations"
+        },
+        "id": "12"
+    },
+    {
+        "title": "horario",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "HORARIO",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "13",
+        "cdate": "2025-01-08T15:37:12-06:00",
+        "udate": "2025-01-08T15:37:12-06:00",
+        "created_timestamp": "2025-01-08 15:37:12",
+        "updated_timestamp": "2025-01-08 15:37:12",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "11"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/13/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/13/relations"
+        },
+        "id": "13"
+    },
+    {
+        "title": "logoUrl",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "LOGOURL",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "14",
+        "cdate": "2025-01-08T15:37:15-06:00",
+        "udate": "2025-01-08T15:37:15-06:00",
+        "created_timestamp": "2025-01-08 15:37:15",
+        "updated_timestamp": "2025-01-08 15:37:15",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "12"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/14/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/14/relations"
+        },
+        "id": "14"
+    },
+    {
+        "title": "data",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "DATA",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "15",
+        "cdate": "2025-01-08T15:37:19-06:00",
+        "udate": "2025-01-08T15:37:19-06:00",
+        "created_timestamp": "2025-01-08 15:37:19",
+        "updated_timestamp": "2025-01-08 15:37:19",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "13"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/15/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/15/relations"
+        },
+        "id": "15"
+    },
+    {
+        "title": "cep",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "CEP",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "16",
+        "cdate": "2025-01-08T15:37:22-06:00",
+        "udate": "2025-01-08T15:37:22-06:00",
+        "created_timestamp": "2025-01-08 15:37:22",
+        "updated_timestamp": "2025-01-08 15:37:22",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "14"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/16/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/16/relations"
+        },
+        "id": "16"
+    },
+    {
+        "title": "codigoRastreio",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "CODIGORASTREIO",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "17",
+        "cdate": "2025-01-08T15:37:25-06:00",
+        "udate": "2025-01-08T15:37:25-06:00",
+        "created_timestamp": "2025-01-08 15:37:25",
+        "updated_timestamp": "2025-01-08 15:37:25",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "15"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/17/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/17/relations"
+        },
+        "id": "17"
+    },
+    {
+        "title": "clientDocument",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "CLIENTDOCUMENT",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "18",
+        "cdate": "2025-01-08T15:37:30-06:00",
+        "udate": "2025-01-08T15:37:30-06:00",
+        "created_timestamp": "2025-01-08 15:37:30",
+        "updated_timestamp": "2025-01-08 15:37:30",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "16"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/18/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/18/relations"
+        },
+        "id": "18"
+    },
+    {
+        "title": "clientPhone",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "CLIENTPHONE",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "19",
+        "cdate": "2025-01-08T15:37:34-06:00",
+        "udate": "2025-01-08T15:37:34-06:00",
+        "created_timestamp": "2025-01-08 15:37:34",
+        "updated_timestamp": "2025-01-08 15:37:34",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "17"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/19/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/19/relations"
+        },
+        "id": "19"
+    },
+    {
+        "title": "clientName",
+        "descript": "",
+        "type": "text",
+        "isrequired": "0",
+        "perstag": "CLIENTNAME",
+        "defval": "",
+        "show_in_list": "0",
+        "rows": "0",
+        "cols": "0",
+        "visible": "1",
+        "service": "",
+        "ordernum": "20",
+        "cdate": "2025-01-08T15:37:38-06:00",
+        "udate": "2025-01-08T15:37:38-06:00",
+        "created_timestamp": "2025-01-08 15:37:38",
+        "updated_timestamp": "2025-01-08 15:37:38",
+        "created_by": "1",
+        "updated_by": "1",
+        "options": [],
+        "relations": [
+            "18"
+        ],
+        "links": {
+            "options": "https://webspy.api-us1.com/api/3/fields/20/options",
+            "relations": "https://webspy.api-us1.com/api/3/fields/20/relations"
+        },
+        "id": "20"
+    }
+].map((item) => {
+    return {
+        field: item.id,
+        value: item.title,
+    }
+})
+console.log(a)
